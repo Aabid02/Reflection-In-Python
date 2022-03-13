a8bc624842d4fa7b51b9d78ae4024b133d0fd401
Aabid <49347539+Aabid02@users.noreply.github.com>
2022-03-13 22:32:23 +0530

    Add files via upload

---
+++ Reflection In Python.py
@@ -0,0 +1,18 @@
+# Python program to illustrate reflection
+def reverse(sequence):
+	sequence_type = type(sequence)
+	empty_sequence = sequence_type()
+	
+	if sequence == empty_sequence:
+		return empty_sequence
+	
+	rest = reverse(sequence[1:])
+	first_sequence = sequence[0:1]
+	
+	# Combining the result
+	final_result = rest + first_sequence
+	
+	return final_result
+
+# Driver code
+print(reverse("Aabid"))
